3c1dc9cfe58cdee5edeacd8724fabf71e0933df4
mkhotamirais <mkhotamirais@gmail.com>
2023-09-07 00:37:13 +0700

    akhir

--- src/app/Store.js
+++ src/app/Store.js
@@ -2,10 +2,10 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import counterReducer from "./counter/Reducer";
 import thunk from "redux-thunk";
 
-let rootReducer = combineReducers({
+let rootReducers = combineReducers({
   counter: counterReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+let store = createStore(rootReducers, composeEnhancer(applyMiddleware(thunk)));
 export default store;
